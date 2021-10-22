a9e40a2b553eceb3a5786c2da96d3c2477a969c8
Lars Almén <lars.almen@afconsult.com>
2021-10-22 10:14:58 +0200

    Small cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,16 +33,7 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        <main>{children}</main>        
       </div>
     </>
   )
